34c36995485bb0e2e3f3f719b621225500d7a082
Esclender <elesclenderlugo@gmail.com>
2024-02-25 11:43:44 -0500

    feat: bottom nav bar (no routing)

--- lib/src/presentation/views/home_screen.dart
+++ lib/src/presentation/views/home_screen.dart
@@ -4,16 +4,15 @@ import 'package:wayllu_project/src/presentation/widgets/bottom_navbar.dart';
 import 'package:wayllu_project/src/utils/constants/colors.dart';
 
 class HomeScreen extends HookWidget {
-  const HomeScreen(Key? key) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgPrimary,
       body: const Placeholder(),
-      bottomNavigationBar: BottomNavBar(
-        key: key,
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: BottomNavBar(),
     );
   }
 }
--- lib/src/presentation/widgets/bottom_navbar.dart
+++ lib/src/presentation/widgets/bottom_navbar.dart
@@ -1,16 +1,82 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:wayllu_project/src/utils/constants/colors.dart';
 
 class BottomNavBar extends HookWidget {
-  const BottomNavBar({super.key});
+  BottomNavBar({super.key});
+
+  final List<IconData> optionsIcons = [
+    Ionicons.home,
+    Ionicons.bar_chart,
+    Ionicons.person,
+  ];
+
+  final viewSelected = useState(0);
+
+  final double blur = 1.5;
+  final BorderRadiusGeometry containersBorder = const BorderRadius.all(
+    Radius.circular(10),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10.0),
-      color: bottomNavBar.withOpacity(0.7),
-      child: const Text('This is the Bottom Navigation Bar'),
+      height: 60,
+      decoration: BoxDecoration(
+        color: bottomNavBar.withOpacity(0.8),
+        borderRadius: containersBorder,
+        border: Border(
+          top: BorderSide(color: bottomNavBarStroke),
+          left: BorderSide(color: bottomNavBarStroke),
+          right: BorderSide(color: bottomNavBarStroke),
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: containersBorder,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: blur,
+            sigmaY: blur,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(
+              optionsIcons.length,
+              (index) => _buildOptions(
+                optionsIcons[index],
+                index,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptions(IconData icon, int index) {
+    return Flexible(
+      child: InkWell(
+        onTap: () {
+          viewSelected.value = index;
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 7.5, vertical: 10.0),
+          decoration: BoxDecoration(
+            color: bottomNavBar,
+            borderRadius: containersBorder,
+            border: Border.all(color: bottomNavBarStroke),
+          ),
+          alignment: Alignment.center,
+          child: Icon(
+            icon,
+            color:
+                viewSelected.value == index ? secondaryColor : noSelectedView,
+          ),
+        ),
+      ),
     );
   }
 }
