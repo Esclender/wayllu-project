c4696ceb87a431b8abd56060d91fd914af0ef1da
jetaypedev <jetaypedev@gmail.com>
2024-03-01 11:43:34 -0500

    refactor: :art: refactor code register user

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:wayllu_project/src/config/theme/app_theme.dart';
-import 'package:wayllu_project/src/presentation/views/auth/register_screen.dart';
 import 'package:wayllu_project/src/presentation/views/home_screen.dart';
 
 void main() {
