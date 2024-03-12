38a44ac3a3567011ca79f761efc63b5957942037
jetaypedev <jetaypedev@gmail.com>
2024-03-11 20:52:03 -0500

    style: :art: widget login field

--- lib/src/presentation/views/login_example.dart
+++ lib/src/presentation/views/login_example.dart
@@ -2,12 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:wayllu_project/src/config/router/app_router.dart';
 import 'package:wayllu_project/src/domain/dtos/user_credentials_rep.dart';
 import 'package:wayllu_project/src/locator.dart';
 import 'package:wayllu_project/src/presentation/cubit/is_admin_cubit.dart';
+import 'package:wayllu_project/src/presentation/widgets/login/text_login.dart';
+import 'package:wayllu_project/src/presentation/widgets/login/text_login_field.dart';
 import 'package:wayllu_project/src/presentation/widgets/register_user/my_text_label.dart';
 import 'package:wayllu_project/src/presentation/widgets/register_user/space_y.dart';
 import 'package:wayllu_project/src/utils/constants/colors.dart';
@@ -45,114 +46,93 @@ class LoginExampleScreen extends HookWidget {
     return Scaffold(
       backgroundColor: bgPrimary,
       body: Container(
-        decoration: new BoxDecoration(
-            gradient: new LinearGradient(
-                colors: [
-                  HexColor('#B800FF'),
-                  HexColor('#FFA743'),
-                  HexColor('#B800FF')
-                ],
-                begin: FractionalOffset.topLeft,
-                end: FractionalOffset.bottomRight,
-                stops: [0.2, 0.4, 0.9],
-                tileMode: TileMode.clamp)),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            stops: [0.01, 1],
+            begin: FractionalOffset.bottomLeft,
+            end: FractionalOffset.topRight,
+            colors: [
+              Color(0xFFFFA743),
+              Color(0xFFB80000),
+            ],
+          ),
+        ),
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          padding:
+              const EdgeInsets.only(top: 40, left: 20, bottom: 40, right: 20),
+          child: ListView(
             children: [
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
+                  Column(
                     children: [
-                      Image.asset('assets/ISOTIPO.PNG'),
+                      Row(
+                        children: [
+                          Image.asset('assets/ISOTIPO.PNG'),
+                        ],
+                      ),
+                      const Row(
+                        children: [
+                          TextLogin(
+                            text: 'BIENVENIDO 👋',
+                          ),
+                        ],
+                      ),
+                      const Row(
+                        children: [
+                          TextLogin(text: 'A '),
+                          TextLogin(
+                            text: 'WAYLLU',
+                            colorText: Colors.amber,
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  Row(
+                  const SpaceY(
+                    value: 40,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('BIENVENIDO 👋'),
+                      const MyTextLabel(hintText: 'DNI'),
+                      const SpaceY(),
+                      TextLoginField(
+                        controller: controllerEmail,
+                        hintText: 'Ingrese su DNI',
+                        obscureText: false,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const MyTextLabel(hintText: 'Password'),
+                      const SpaceY(),
+                      TextLoginField(
+                        controller: controllerClave,
+                        hintText: 'Ingrese con su correo',
+                        obscureText: true,
+                      ),
+                      const SpaceY(),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.58),
+                          ),
+                          backgroundColor: HexColor('#B80000'),
+                          minimumSize: const Size.fromHeight(60),
+                        ),
+                        onPressed: () => _loginEvent(
+                          controllerEmail.text,
+                          controllerClave.text,
+                          context,
+                        ),
+                        child: const Text(
+                          'Ingresar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
                     ],
-                  ),
-                  Row(children: [
-                    Text('A '),
-                    Text('WAYLLU'),
-                  ])
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  MyTextLabel(hintText: 'Correo'),
-                  SpaceY(),
-                  TextFormField(
-                    controller: controllerEmail,
-                    decoration: InputDecoration(
-                      hintText: 'Escriba su mail',
-                      fillColor: Colors.transparent,
-                      contentPadding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                      hintStyle: GoogleFonts.poppins(
-                        fontSize: 15,
-                        color: const Color.fromARGB(128, 0, 0, 0),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(120, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(120, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      filled: true,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  MyTextLabel(hintText: 'Clave'),
-                  SpaceY(),
-                  TextFormField(
-                    controller: controllerClave,
-                    decoration: InputDecoration(
-                      hintText: 'Escriba su clave',
-                      fillColor: Colors.transparent,
-                      contentPadding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                      hintStyle: GoogleFonts.poppins(
-                        fontSize: 15,
-                        color: const Color.fromARGB(128, 0, 0, 0),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(120, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(120, 0, 0, 0)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      filled: true,
-                    ),
-                  ),
-                  SpaceY(),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.58),
-                      ),
-                      backgroundColor: HexColor('#B80000'),
-                      minimumSize: const Size.fromHeight(60),
-                    ),
-                    onPressed: () => _loginEvent(
-                      controllerEmail.text,
-                      controllerClave.text,
-                      context,
-                    ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(color: Colors.white),
-                    ),
                   ),
                 ],
               ),
