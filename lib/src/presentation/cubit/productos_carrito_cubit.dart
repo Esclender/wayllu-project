c232ae6097e7482e26279dcd249eb7a6372abcc5
unknown <rachel11072003@gmail.com>
2024-04-09 20:02:52 -0500

    guardando cambios 09/04/2024

---
+++ lib/src/presentation/cubit/productos_carrito_cubit.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: parameter_assignments
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wayllu_project/src/domain/models/carrito_item.dart';
+import 'package:wayllu_project/src/domain/models/products_info/product_info_model.dart';
+/*
+class ProductsCarrito extends Cubit<List<CarritoItem>> {
+  ProductsCarrito() : super([]);
+
+  Future<void> addNewProductToCarrito({
+    required ProductInfo product,
+    int quantity = 0,
+  }) async {
+    final productToAdd = CarritoItem(
+      info: product,
+      quantity: quantity,
+      decrease: () {
+        addNewProductToCarrito(product: product, quantity: quantity++);
+      },
+      increase: () {
+        if (quantity > 0) {
+          addNewProductToCarrito(product: product, quantity: quantity--);
+        }
+      },
+    );
+
+    emit(
+      [
+        ...super.state,
+        productToAdd,
+      ],
+    );
+  }
+}
+*/
