cad8fca83c88383f0af81f836f5b7d0ab1b48b48
Esclender <elesclenderlugo@gmail.com>
2024-08-18 10:32:35 -0500

    refactor: Remove unused code and fix typo in carrito_item.dart

--- lib/src/domain/models/carrito_item.dart
+++ lib/src/domain/models/carrito_item.dart
@@ -10,13 +10,13 @@ class CarritoItem {
     required this.info,
     required this.quantity,
   });
+
   double _calculatePrecioVenta() {
     return info.PRECIO * quantity;
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'COD_ARTESANA': info.COD_ARTESANA,
       'COD_ARTESANA': info.COD_ARTESANA,
       'COD_PRODUCTO': info.COD_PRODUCTO,
       'CATEGORIA_PRODUCTO': info.CATEGORIA,
